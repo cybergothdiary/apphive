c28ba0f7a12222d1aa8662a42b1f54cb50fe8a42
cybergothdiary <sashaanrad@gmail.com>
2023-01-04 03:21:52 +0200

    code refactoring stage #1

--- responsive-menu.js
+++ responsive-menu.js
@@ -1,6 +1,19 @@
+const header = document.querySelector('.header');
 const navMenu = document.querySelector('.nav-menu');
+const logotype = document.querySelector('.logotype-block');
 const navIcon = document.querySelector('.nav-icon span');
 
 navIcon.addEventListener('click', () => {
+    header.classList.toggle('header-pt_toggler');
     navMenu.classList.toggle('nav-menu_toggler');
+    logotype.classList.toggle('nav-logotype_toggler');
+    navIcon.classList.toggle('nav-icon_toggler');
+
+    if(navMenu.classList.contains('nav-menu_toggler')) {
+        navIcon.textContent = '×';
+        navIcon.style.fontSize = '2rem';
+        navIcon.style.opacity = '.45';
+    } else {
+        navIcon.textContent = '☰';
+    }
 })
